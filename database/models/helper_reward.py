23017ae70a873c807a59b98237483062d96d9573
Michał Wiliński <52979291+JanekDev@users.noreply.github.com>
2022-04-08 19:12:17 +0200

    Update helper_reward.py

--- database/models/helper_reward.py
+++ database/models/helper_reward.py
@@ -6,4 +6,4 @@ class HelperReward(Base):
     __tablename__ = 'helper_rewards'
 
     reward = Column('reward', Integer, primary_key=True)
-    emoji_name = Column('emoji', String, nullable=False)
+    emoji_name = Column('emoji_name', String, nullable=False)
