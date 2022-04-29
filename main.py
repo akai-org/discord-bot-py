cc2681682e9e2581e5a488db965598a887655fd5
Dmytro-Romaniv <dm.romaniv@gmail.com>
2022-04-29 18:50:48 +0200

    Implement ranking

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from services.helper import HelperService
 from services.role_channels import RoleChannels
 from services.message_to_role import MessageToRoleService
 from services.thread import ThreadService
+from services.ranking import RankingService
 from services.util.request import RequestUtilService
 
 WRITE_FILE_MODE = 'w'
@@ -85,9 +86,10 @@ if __name__ == '__main__':
     role_channels = RoleChannels(logger, settings_repository, message_to_role_repository, request_util)
     command_service = CommandService(command_repository, helper_service, role_channels, logger, helper_repository)
     thread_service = ThreadService(logger, request_util)
+    ranking_service = RankingService(logger, command_repository, helper_repository)
 
     bot = AkaiBot(logger, settings_repository, command_service, message_to_role_service, message_to_role_repository,
-                  helper_service, helper_repository, thread_service)
+                  helper_service, helper_repository, thread_service, ranking_service)
 
     if DISCORD_LOG_CHANNEL_ID is not None:
         discord_handler = log_handling.DiscordHandler(bot, int(DISCORD_LOG_CHANNEL_ID))
