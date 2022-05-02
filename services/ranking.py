6576e3cfd7f4dc909d3fd3b09c7aefaa821915e9
Dmytro-Romaniv <52633228+Dmytro-Romaniv@users.noreply.github.com>
2022-05-02 02:10:05 +0200

    Osobna funkcja dla tworzenia wiadomości

--- services/ranking.py
+++ services/ranking.py
@@ -18,30 +18,20 @@ class RankingService:
         if not hasattr(self, 'anchor'):
             self.logger.debug('Ranking has not been set up.')
             return
-        info = ''
-        place = 1
-        for rank in self.helper_repository.get_whole_rank():
-            if not hasattr(self.guild.get_member(rank.user_id), 'name') or rank.points == 0:
-                continue
-            info += (f'{":first_place:" if place == 1 else ":second_place:" if place == 2 else ":third_place:" if place == 3 else "`"+str(place)+".`"} '
-                     f'**{self.guild.get_member(rank.user_id).name}** — {rank.points} {"point" if rank.points == 1 else "points"}\n')
-            place += 1
+        info = self.generate()
         embed = discord.Embed(title = '\n', description = info, color=0xFAA21B, timestamp=datetime.now())
         embed.set_author(name = 'RANKING:')
         embed.set_thumbnail(url = '')     # można dodać zdjęcie
         embed.set_footer(text = 'Last updated', icon_url = 'https://i.imgur.com/T6PhftD.png')
         await self.anchor.edit(content = '** **', embed = embed)
-
-    async def setup(self, bot, message: discord.Message):
-        if message.content == "$setup":
-            self.channel = message.channel
-            self.logger.debug('Setting up the ranking...')
-            await message.delete()
-            await self.channel.send('Loading...')
-
-        if message.author == bot.user:
-            self.anchor = message
-            self.guild = message.guild
-            self.logger.debug('Ranking has been set up.')
-            await self.update()
         
+    def generate(self):
+        data = ''
+        place = 1
+        for rank in self.helper_repository.get_whole_rank():
+            if not hasattr(self.guild.get_member(rank.user_id), 'name') or rank.points == 0:
+                continue
+            data += (f'{":first_place:" if place == 1 else ":second_place:" if place == 2 else ":third_place:" if place == 3 else "`"+str(place)+".`"} '
+                     f'**{self.guild.get_member(rank.user_id).name}** — {rank.points} {"point" if rank.points == 1 else "points"}\n')
+            place += 1
+        return data
