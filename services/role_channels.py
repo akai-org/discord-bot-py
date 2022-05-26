981e7fdb863e7fdd162162a1148673574a89d040
BbqGamer <korba.adam@gmail.com>
2022-05-26 18:43:30 +0200

    Change help messages and add parameters to project creation

--- services/role_channels.py
+++ services/role_channels.py
@@ -7,7 +7,7 @@ from database.repositories.message_to_role import MessageToRoleRepository
 from services.util.request import RequestUtilService
 
 ROLE_TYPES = {
-    'projekt': {'name': 'project', 'id': 'project_channel_id'},
+    'projekt': {'name': 'projekt', 'id': 'project_channel_id'},
     'tech': {'name': 'technology', 'id': 'technology_channel_id'},
 }
 
@@ -25,13 +25,16 @@ class RoleChannels:
 
     async def handle_role_channel(self, message, command):
         if len(command["args"]) == 0:
-            await message.reply(f'USAGE: \n\t{command["name"]} <role_names>\t      - to create roles \n\t{command["name"]} <role_names> -d\t - to delete roles')
+            if(command["name"] == "projekt"):
+                await message.reply(f"""HELP: \n\t$projekt <nazwy projektów> -m <opis projektu> -p <projekt managerzy> -t <tech stack>\n\t$projekt <nazwy projektów> -d\t          - usuń projekt""")
+            else:
+                await message.reply(f'HELP: \n\t$tech <nazwy technologii>\t          - utwórz technologię \n\t$tech <nazwy technologii> -d\t          - usuń technologie')
 
         for role_name in command['args']:
-            if len(command['params']) == 0:
-                await self.create_role_channel(message, command, role_name)
-            elif "d" in command['params']:
+            if "d" in command['params']:
                 await self.delete_role_channel(message, command, role_name)
+            else:
+                await self.create_role_channel(message, command, role_name)
 
 
     async def create_role_channel(self, message, command, role_name):
@@ -47,8 +50,14 @@ class RoleChannels:
 
             url = f"/channels/{channel_id}/messages"
             data = {
-                "content": f'New {role_type} {role_name} appeared!'
+                "content": f'{role_type}: **{role_name}**\n'
             }
+
+            if role_type == 'projekt':
+                data['content'] += ' '.join(command["params"]["m"]) if "m" in command["params"] else ''
+                data['content'] += "\nPM: " + ' '.join(command["params"]["p"]) if "p" in command["params"] else ''
+                data['content'] += "\nTech stack: " + ' '.join(command["params"]["t"]) if "t" in command["params"] else ''
+
             response = self.request_util.make_post(data, url)
 
             self.message_to_role_repo.create_message_role_association(response['id'], role.id)
@@ -65,7 +74,7 @@ class RoleChannels:
         channel = guild.get_channel(channel_id)
         messages = await channel.history(limit=1000).flatten()
         for m in messages:
-            if(role_name in m.content.split()):
+            if(role_name == m.content.split()[1][2:-2]):
                 self.logger.debug(f'Deleting {role_name} from {channel.name}')
                 await m.delete()
 
